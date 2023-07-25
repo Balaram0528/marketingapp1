694c5614f6bf185023321d2452421b52cdb70552
Balaram0528 <137981671+Balaram0528@users.noreply.github.com>
2023-07-25 17:19:07 +0530

    Add files via upload

---
+++ Marketingapp1Application.java
@@ -0,0 +1,13 @@
+package com.marketingapp1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Marketingapp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Marketingapp1Application.class, args);
+	}
+
+}
